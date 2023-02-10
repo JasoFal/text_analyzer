a003aafcc85799dbf057310d3cfc3b40c973e864
Jason Falk <jasonfalk9@gmail.com>
2023-02-10 15:58:47 -0800

    Add form input html code.

--- js/scripts.js
+++ js/scripts.js
@@ -35,3 +35,19 @@ function removeBannedWords(word ,text) {
 });
 return(textArray);
 }
+
+// UI Logic //
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const passage = document.getElementById("text-passage").value;
+  const word = document.getElementById("word").value;
+  const wordCount = wordCounter(passage);
+  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+  document.getElementById("total-count").innerText = wordCount;
+  document.getElementById("selected-count").innerText = occurrencesOfWord;
+}
+
+window.addEventListener("load", function() {
+  this.document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
